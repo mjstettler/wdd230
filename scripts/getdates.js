62178dd53983459246a13213b80f325c9dc83cbf
Mike Stettler <mjstettler@gmail.com>
2023-09-16 16:18:26 -0600

    Completed the first assignment to the best of my abilities

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+const currentYear = new Date();
+document.getElementById("currentyear").innerText = currentYear.getFullYear();
+
+document.getElementById("lastModified").innerText = document.lastModified
