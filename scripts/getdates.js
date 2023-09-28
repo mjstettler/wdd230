1d8b5449815ee9d1dc382b2aff0b2613abeeb0a1
Mike Stettler <mjstettler@gmail.com>
2023-09-28 12:23:54 -0600

    Hamburger menu added, and iframe map

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,11 @@
+const hamButton = document.querySelector('#menu')
+const navigation = document.querySelector('.navigation')
 
 document.getElementById("currentyear").innerText = new Date().getFullYear();
 
 document.getElementById("lastModified").innerText = `Last Modified: ${document.lastModified}`
+
+hamButton.addEventListener('click', ()=> {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+})
