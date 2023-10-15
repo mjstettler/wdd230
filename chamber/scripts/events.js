33bc0861fc3800aae70bb0e8729de026c953723e
Mike Stettler <mjstettler@gmail.com>
2023-10-14 22:30:17 -0600

    Added darkmode, finished layout of footer, created main grids

--- chamber/scripts/events.js
+++ chamber/scripts/events.js
@@ -1,7 +1,23 @@
 const hamButton = document.querySelector('#menu')
 const navigation = document.querySelector('.navbar')
+const darkButton = document.querySelector('#darkButton')
+const Icon = document.querySelector('#darkIcon')
+
 
 hamButton.addEventListener('click', ()=> {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
+    
+})
+
+darkButton.addEventListener('click', ()=> {
+    if (Icon.classList.value == 'fa fa-sun-o') {
+    darkIcon.classList.value = 'fa fa-moon-o';
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    } else  {
+        darkIcon.classList.value = 'fa fa-sun-o';
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+    }
 })
