e51798306c95fcf47f1f21873e2307d15280e86a
mjstettler <mjstettler@gmail.com>
2023-10-20 16:49:07 -0600

    finished week06 assignment

--- chamber/scripts/events.js
+++ chamber/scripts/events.js
@@ -1,8 +1,19 @@
 const hamButton = document.querySelector('#menu')
 const navigation = document.querySelector('.navbar')
-const darkButton = document.querySelector('#darkButton')
-const Icon = document.querySelector('#darkIcon')
+const darkButton = document.querySelector('#darkButton').textContent
+const darkMode = document.querySelector("#darkButton")
 
+// Variables for changing border color 
+const box1 = document.querySelector('.weather');
+const box2 = document.querySelector('.events');
+const box3 = document.querySelector('#spotlight1');
+const box4 = document.querySelector('#spotlight2');
+const box5 = document.querySelector('#spotlight3');
+const box6 = document.querySelector('#spotlight4');
+    
+const boxes = new Array(box1,box2, box3, box4,box5,box6);
+
+console.log(darkButton)
 
 hamButton.addEventListener('click', ()=> {
     navigation.classList.toggle('open');
@@ -10,14 +21,20 @@ hamButton.addEventListener('click', ()=> {
     
 })
 
-darkButton.addEventListener('click', ()=> {
-    if (Icon.classList.value == 'fa fa-sun-o') {
-    darkIcon.classList.value = 'fa fa-moon-o';
+darkMode.addEventListener('click', ()=> {
+    if (darkButton == "🔆") {
+    document.querySelector('#darkButton').textContent = "🌙";
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
-    } else  {
-        darkIcon.classList.value = 'fa fa-sun-o';
+
+    boxes.forEach((box) => {
+        box.style.borderColor = "white";
+    });
+
+    } else {
+        document.querySelector("#darkButton").textContent = '🔆';
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
     }
+    console.log(darkButton)
 })
