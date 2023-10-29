818a0fddba422236f255f0e60999508c34a6290c
Mike Stettler <mjstettler@gmail.com>
2023-10-28 23:36:44 -0600

    attempted to get js date working

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -2,12 +2,24 @@ document.getElementById("lastModified").innerText = `Last Modified: ${document.l
 
 document.getElementById("currentyear").innerText = new Date().getFullYear();
 
-const today = new Date();
-const lastVisit = '';
+const msInDay = 84600000;
+const today = Date.now();
+const lastVisit = localStorage.getItem('visit');
+const daysAgo = today-lastVisit;
 
+console.log(daysAgo);
 console.log(today);
+localStorage.setItem('visit', today)
 
-if (today == lastVisit) {
+if (lastVisit == 0) {
     document.querySelector('#date').textContent = 'Welcome! Let us know if you have an questions.';
-}
+} else if (lastVisit < msInDay) {
+    document.querySelector('#date').textContent = 'Back so soon! Awesome!';
+} else {
+    if (lastVisit > (lastVisit+msInDay) && lastVisit < (lastVisit+msInDay*2)) {
+        document.querySelector('#date').textContent = 'You last visited '+ lastVisit + ' day ago.';
+    } else {
+        document.querySelector('#date').textContent = 'You last visited ' + lastVisit + ' days ago.';
+    }
+};
 
