fd559442c853f3ae7ff96f08f506c860a59ce8f7
Mike Stettler <mjstettler@gmail.com>
2023-11-01 17:25:49 -0600

    updating links and hero img

--- chamber/scripts/events.js
+++ chamber/scripts/events.js
@@ -1,6 +1,6 @@
 const hamButton = document.querySelector('#menu')
 const navigation = document.querySelector('.navbar')
-const darkButton = document.querySelector('#darkButton').textContent
+const darkButton = document.querySelector('#darkButton')
 const darkMode = document.querySelector("#darkButton")
 
 // Variables for changing border color 
@@ -10,31 +10,31 @@ const box3 = document.querySelector('#spotlight1');
 const box4 = document.querySelector('#spotlight2');
 const box5 = document.querySelector('#spotlight3');
 const box6 = document.querySelector('#spotlight4');
-    
-const boxes = Array(box1,box2,box3,box4,box5,box6);
 
-console.log(darkButton)
+const boxes = [box1, box2, box3, box4, box5, box6];
 
-hamButton.addEventListener('click', ()=> {
+console.log(darkButton.textContent)
+
+hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
-    
+
 })
 
-darkMode.addEventListener('click', ()=> {
-    if (darkButton == "🔆") {
-    document.querySelector('#darkButton').textContent = "🌙";
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
+darkMode.addEventListener('click', () => {
+    if (darkButton.textContent == "🔆") {
+        darkButton.textContent = "🌙";
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
 
-    boxes.forEach((box) => {
-        box.style.borderColor = "white";
-    });
+        boxes.forEach((box) => {
+            box.style.borderColor = "white";
+        });
 
     } else {
-        document.querySelector("#darkButton").textContent = '🔆';
+        darkButton.textContent = '🔆';
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
     }
-    console.log(darkButton)
+    console.log(darkButton.textContent)
 })
