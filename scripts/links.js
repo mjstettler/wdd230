c4a6fcffec0f033b1420be4ea971606b11cb1cfe
Mike Stettler <mjstettler@gmail.com>
2023-11-08 20:22:24 -0700

    Links js and Json created

---
+++ scripts/links.js
@@ -0,0 +1,8 @@
+const url = "https://mjstettler.github.io/wdd230/scripts/links.js";
+
+async function getLinks() {
+    const response = await fetch(url);
+    const links = await response.json();
+    console.log(data)
+
+}
