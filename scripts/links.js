6f2524919e55d90e9b09c85e5d0dc81ee9b0bb2d
Mike Stettler <mjstettler@gmail.com>
2023-11-08 20:45:06 -0700

    correct url error

--- scripts/links.js
+++ scripts/links.js
@@ -1,9 +1,22 @@
-const url = "https://mjstettler.github.io/wdd230/scripts/links.js";
+const url = "https://mjstettler.github.io/wdd230/scripts/links.json";
+const links = document.querySelector('#links');
 
 async function getLinks() {
     const response = await fetch(url);
-    const links = await response.json();
-    console.log(data)
+    const data = await response.json();
+    console.log(data);
+
+    displayLinks(data)
+}
+
+const displayLinks = (weeks) => {
+    weeks.foreach(week => {
+        let li = document.createElement('li');
+        let link = document.createElement('a');
+        link.textContent = week.title;
+        link.setAttribute('href', week.url)
+
+    })
 }
 
 getLinks();
