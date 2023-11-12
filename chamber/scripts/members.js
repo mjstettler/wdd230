7bfd5787d746fa68695d3156b861380cb5eb6cf2
Mike Stettler <mjstettler@gmail.com>
2023-11-11 20:49:06 -0700

    directory html, json, js draft done

---
+++ chamber/scripts/members.js
@@ -0,0 +1,52 @@
+const cards = document.querySelector("#directoryCards");
+const url = "https://mjstettler.github.io/wdd230/chamber/data/members.json";
+
+function displayMembers(members) {
+    members.forEach((member) => {
+        let cardElement = document.createElement('div');
+        let name = document.createElement('h2');
+        let phoneNum = document.createElement('p');
+        let website = document.createElement('a');
+        let logo = document.createElement('img');
+        let level = document.createElement('p');
+        let opHours = document.createElement('p');
+
+        cardElement.setAttribute('class', 'card')
+        name.textContent = member.business;
+        phoneNum.textContent = member.phone;
+        website.textContent = member.url;
+        website.setAttribute('href', member.url);
+        logo.setAttribute('src', member.image);
+        logo.setAttribute('alt', `${member.business} logo`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '300');
+        logo.setAttribute('height', 'auto');
+        level.textContent = member.memberLevel;
+        opHours.textContent = member.hours;
+
+        cardElement.appendChild(logo, name)
+
+        member.address.forEach((building) => {
+            let locationElement = document.createElement('p');
+            locationElement.textContent = building.location;
+
+            cardElement.appendChild(locationElement)
+        });
+
+        cardElement.appendChild(phone, opHours, website)
+
+        cards.appendChild(cardElement);
+    });
+}
+
+
+async function getMembers() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json;
+
+        displayMembers(data.commerceMembers)
+    } catch (error) {
+        console.error('Error fetching or parsing data:', error);
+    }
+}
--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,5 +1,6 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
+
 const displayProphets = (prophets) => {
     prophets.forEach(prophet => {
         let card = document.createElement('section');
