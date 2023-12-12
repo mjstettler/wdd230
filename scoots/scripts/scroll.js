cf382c9af77862152e53bd1f6f17634e903dbddb
Mike Stettler <mjstettler@gmail.com>
2023-12-12 13:14:55 -0700

    form work, button to top

---
+++ scoots/scripts/scroll.js
@@ -0,0 +1,20 @@
+const scrollBtn = document.getElementById("scrollToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 400) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
